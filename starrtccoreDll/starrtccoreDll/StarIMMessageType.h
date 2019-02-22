8f7dee35d1544169db383c504f7cba162119d3f1
starrtc <support@starrtc.com>
2019-02-22 11:17:33 +0800

    windows demo for starrtc

--- starrtccoreDll/starrtccoreDll/StarIMMessageType.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define MSG_SEND_TYPE_ON_LINE_NO_PUSH	0	//（用户不在线时,不推送、不入库）
-#define MSG_SEND_TYPE_OFF_LINE_NO_PUSH	1	//（用户不在线时,不推送、入库）
-#define MSG_SEND_TYPE_OFF_LINE_PUSH		2	//（用户不在线时,推送、入库）voipCalling
-#define MSG_SEND_TYPE_ON_LINE_PUSH		3	//（用户不在线时,推送、不入库）
-
-#define MSG_TYPE_CONTROL				0	//SDK内部 控制消息
-#define MSG_TYPE_CUSTOMER				1	//用户自定义消息
